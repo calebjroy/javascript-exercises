5501421882b7b4d58a8e5d032a8a3430162c3f8f
Roy <42874012+calebjroy@users.noreply.github.com>
2022-12-22 19:22:51 -0700

    Add solution to 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = [];
+    for (let char in string) {
+        newString.unshift(string[char]);
+    }
+    return newString.join('');
 };
 
 // Do not edit below this line
