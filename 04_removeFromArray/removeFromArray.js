e7c1e6dc3064427e80537baa40903ecabb7a7a77
Roy <42874012+calebjroy@users.noreply.github.com>
2022-12-24 16:16:58 -0700

    Add solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayToModify) {
+    let index;
+    let argument;
+    
+    for (let i=1; i < arguments.length; i++) {
+        argument = arguments[i];
+        if (arrayToModify.includes(argument)) {
+            index = arrayToModify.indexOf(argument)
+            arrayToModify.splice(index,1);
+            // console.log(`Arguments: ${JSON.stringify(arguments)}\nArgument: ${argument}, Index: ${index}, Array: ${arrayToModify}`)
+        }  
+    }
+    return arrayToModify;
 };
 
 // Do not edit below this line
