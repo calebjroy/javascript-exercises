8106a36e6f7d36c1282202c837ede9022d737bb1
Roy <42874012+calebjroy@users.noreply.github.com>
2022-12-24 16:48:07 -0700

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    // Input validation
+    if ((val1 < 0 || val2 < 0) || (typeof(val1) != 'number' || typeof(val2) != 'number')) {
+        return 'ERROR';
+    }
 
+    if (val1 < val2) {
+        smallerVal = val1;
+        largerVal = val2;
+    } else {
+        smallerVal = val2;
+        largerVal = val1;
+    }
+
+    // Process values
+    const numOfElements = largerVal - smallerVal + 1;
+    let arrayToSum = Array.from({length: numOfElements}, (x, i) => i+1);
+    let sum = arrayToSum.reduce((previousVal, currentVal) => previousVal + currentVal, 0);
+    return sum;
 };
 
 // Do not edit below this line
