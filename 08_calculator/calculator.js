5b9e368aebaa60fd24a5fa3bf4be2d553a4422ee
Roy <42874012+calebjroy@users.noreply.github.com>
2022-12-27 01:00:31 -0700

    Add solution for 08_Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNums) {
+	return arrayOfNums.reduce((accumulator, currentVal) => accumulator + currentVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfNums) {
+  return arrayOfNums.reduce((accumulator, currentVal) => accumulator * currentVal, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0 || typeof(num) != 'number') {
+    return 'ERROR';
+  } else if (num == 0) {
+    return 1;
+  }
+
+  let newArray = Array.from({length: num}, (x, i) => i+1);
+  return newArray.reduce((previousVal, currentVal) => previousVal * currentVal, 1);
 };
 
 // Do not edit below this line
